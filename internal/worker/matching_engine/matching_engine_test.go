fe71c77f4e8429121b7941e92c2b4ed7cdfcaf3e
Harry Lu <harry.nan.825@gmail.com>
2025-01-17 13:58:04 +0800

    test: MatchingEngine Test PlaceOrder - BuySide

--- internal/worker/matching_engine/matching_engine_test.go
+++ internal/worker/matching_engine/matching_engine_test.go
@@ -132,3 +132,165 @@ func (s *MatchingEngineTestSuite) TestPlaceOrder_BuyOrder_PriceAndQuantityAreFul
 		CreatedAt:   nowTime,
 	}, matchingEvents[0])
 }
+
+func (s *MatchingEngineTestSuite) TestPlaceOrder_BuyOrder_MatchFromLowerPriceToHigherPrice() {
+	// arrange
+	nowTime, _ := time.Parse(time.RFC3339, "2024-01-01T00:00:00Z08:00")
+	price := 25.89
+
+	// add sell orders
+	sellOrder1 := Order{
+		ID:                uuid.NewString(),
+		Symbol:            s.symbol,
+		Side:              SideSELL,
+		Price:             price - 1,
+		Quantity:          10,
+		RemainingQuantity: 10,
+		CanceledQuantity:  0,
+		CreatedAt:         nowTime,
+		UpdatedAt:         nowTime,
+	}
+
+	sellOrder2 := sellOrder1
+	sellOrder2.ID = uuid.NewString()
+	sellOrder2.Price = price - 2
+	sellOrder2.CreatedAt = nowTime.Add(1 * time.Minute)
+	sellOrder2.UpdatedAt = nowTime.Add(1 * time.Minute)
+
+	sellOrder3 := sellOrder1
+	sellOrder3.ID = uuid.NewString()
+	sellOrder3.Price = price - 1
+	sellOrder3.CreatedAt = nowTime.Add(1 * time.Minute)
+	sellOrder3.UpdatedAt = nowTime.Add(1 * time.Minute)
+
+	sellOrder4 := sellOrder1
+	sellOrder4.ID = uuid.NewString()
+	sellOrder4.Price = price
+	sellOrder4.CreatedAt = nowTime.Add(1 * time.Minute)
+	sellOrder4.UpdatedAt = nowTime.Add(1 * time.Minute)
+
+	orderBook := NewOrderBook()
+	orderBook.AddOrder(&sellOrder1)
+	orderBook.AddOrder(&sellOrder2)
+	orderBook.AddOrder(&sellOrder3)
+
+	// init buy order
+	buyOrder1 := sellOrder1
+	buyOrder1.ID = uuid.NewString()
+	buyOrder1.Price = price
+	buyOrder1.Quantity = 30
+	buyOrder1.RemainingQuantity = 30
+	buyOrder1.Side = SideBUY
+
+	// mock uuid
+	uuidStr := uuid.NewString()
+	getUUID = func() string {
+		return uuidStr
+	}
+
+	// mock now
+	now = func() time.Time {
+		return nowTime
+	}
+
+	// act
+	matchingEngine := NewMatchingEngine(orderBook)
+	matchingEvents := matchingEngine.PlaceOrder(&buyOrder1)
+
+	// assert
+	// assert matched orders not exist
+	s.Equal(&sellOrder4, orderBook.sellHead.Next)
+	// assert matching events
+	s.Equal(3, len(matchingEvents))
+	s.Equal(events.MatchingTransaction{
+		ID:          uuidStr,
+		Symbol:      s.symbol,
+		BuyOrderID:  buyOrder1.ID,
+		SellOrderID: sellOrder2.ID,
+		Price:       sellOrder2.Price,
+		Quantity:    sellOrder2.Quantity,
+		CreatedAt:   nowTime,
+	}, matchingEvents[0])
+	s.Equal(events.MatchingTransaction{
+		ID:          uuidStr,
+		Symbol:      s.symbol,
+		BuyOrderID:  buyOrder1.ID,
+		SellOrderID: sellOrder1.ID,
+		Price:       sellOrder1.Price,
+		Quantity:    sellOrder1.Quantity,
+		CreatedAt:   nowTime,
+	}, matchingEvents[1])
+	s.Equal(events.MatchingTransaction{
+		ID:          uuidStr,
+		Symbol:      s.symbol,
+		BuyOrderID:  buyOrder1.ID,
+		SellOrderID: sellOrder3.ID,
+		Price:       sellOrder3.Price,
+		Quantity:    sellOrder3.Quantity,
+		CreatedAt:   nowTime,
+	}, matchingEvents[2])
+}
+
+func (s *MatchingEngineTestSuite) TestPlaceOrder_BuyOrder_InsertRemainingOrderToOrderBook() {
+	// arrange
+	nowTime, _ := time.Parse(time.RFC3339, "2024-01-01T00:00:00Z08:00")
+	price := 25.89
+	var sellQuantity, buyQuantity int64 = 10, 30
+
+	// add sell orders
+	sellOrder1 := Order{
+		ID:                uuid.NewString(),
+		Symbol:            s.symbol,
+		Side:              SideSELL,
+		Price:             price,
+		Quantity:          sellQuantity,
+		RemainingQuantity: sellQuantity,
+		CanceledQuantity:  0,
+		CreatedAt:         nowTime,
+		UpdatedAt:         nowTime,
+	}
+
+	orderBook := NewOrderBook()
+	orderBook.AddOrder(&sellOrder1)
+
+	// init buy order
+	buyOrder1 := sellOrder1
+	buyOrder1.ID = uuid.NewString()
+	buyOrder1.Price = price
+	buyOrder1.Quantity = buyQuantity
+	buyOrder1.RemainingQuantity = buyQuantity
+	buyOrder1.Side = SideBUY
+
+	// mock uuid
+	uuidStr := uuid.NewString()
+	getUUID = func() string {
+		return uuidStr
+	}
+
+	// mock now
+	now = func() time.Time {
+		return nowTime
+	}
+
+	// act
+	matchingEngine := NewMatchingEngine(orderBook)
+	matchingEvents := matchingEngine.PlaceOrder(&buyOrder1)
+
+	// assert
+	// assert matched orders not exist
+	s.Equal(orderBook.sellHead.Next, orderBook.sellTail)
+	// assert matching events
+	s.Equal(1, len(matchingEvents))
+	s.Equal(events.MatchingTransaction{
+		ID:          uuidStr,
+		Symbol:      s.symbol,
+		BuyOrderID:  buyOrder1.ID,
+		SellOrderID: sellOrder1.ID,
+		Price:       sellOrder1.Price,
+		Quantity:    sellOrder1.Quantity,
+		CreatedAt:   nowTime,
+	}, matchingEvents[0])
+	// assert the remaining buy order is in the order book
+	s.Equal(&buyOrder1, orderBook.buyHead.Next.headOrder.Next)
+	s.Equal(int64(20), buyOrder1.RemainingQuantity)
+}
